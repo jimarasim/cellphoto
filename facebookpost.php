f380f82e07fd8679ccde223d7563995907cc0fab
jimarasim <jaemzware@hotmail.com>
2014-04-16 17:37:12 -0700

    took out some comments

--- facebookpost.php
+++ facebookpost.php
@@ -1,11 +1,8 @@
 <?php
 include '../facebook-php-sdk/src/facebook.php'; 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-require_once("facebook.php");
+ 
+require_once('../facebook-php-sdk/src/facebook.php');
+
 
 $config = array(
             'appId' => '709181395771740',
