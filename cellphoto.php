0d1b1a243d4f239427d66a57fbaf603f1635e35f
jimarasim <jaemzware@hotmail.com>
2014-07-15 07:32:11 -0700

    commented out new implementation to post to facebook.  will work on later

--- cellphoto.php
+++ cellphoto.php
@@ -1,3 +1,6 @@
+<?php
+    include 'cellphotomail.class'; 
+?>
 <html>
 <head>
     <title>cellphoto</title>
@@ -6,12 +9,17 @@
     <!--NOTE: /CELLPHOTO, /CELLPHOTO/CELLPHOTO, AND /CELLPHOTO/CELLPHOTO/THUMBS NEED TO BE WRITEABLE-->
     <H1><A href="cellphotoview.php">View Cell Photos</a></h1>
 <?php
-	include 'cellphotomail.class'; 
+	
 	echo('CELLPHOTO<BR />');
         
+        //get a cell photo mail object
 	$myCellPhotoMail = new CellPhotoMail(null,null,null,null,null);
         
+        //check for and publish photos
         $myCellPhotoMail->ProcessPhotos();
+        
+        //post to facebook (see docs, downloaded facebook sdk)
+        //FacebookSession::setDefaultApplication('YOUR_APP_ID', 'YOUR_APP_SECRET');
 	
         
 ?>
