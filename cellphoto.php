c05d04e5271a3c86580ae7844fba12a618a17f15
jimarasim <jaemzware@hotmail.com>
2014-07-26 03:35:41 -0700

    got json string returned, of images

--- cellphoto.php
+++ cellphoto.php
@@ -1,29 +1,44 @@
 <?php
     include 'cellphotomail.class'; 
-?>
-<html>
-<head>
-    <title>cellphoto</title>
     
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-</head>
-<body>
+    $jsonImageLinks=filter_input(INPUT_GET,('jsonImageLinks'));
+    
+    if(isset($jsonImageLinks)){
+        getJsonImageLinks();
+    }
+    else{
+        processPhotosOnly();
+    }
+        
+    /**
+     * This function will retrieve and save images, and return a json array of images saved
+     */
+    function getJsonImageLinks(){
+//        echo('{"jsonarray":["key1":"value1","key2":"value2"]}');
+        //get a cell photo mail object
+	$myCellPhotoMail = new CellPhotoMail(null,null,null);
+        
+        //check for and publish photos, and return json array of images
+        echo($myCellPhotoMail->getJsonImagePaths());
+    }
+    
+    /**
+     * This function will retrieve and save images, and produce html output
+     */
+    function processPhotosOnly(){
+        //html header
+        echo("<html><head><title>cellphoto</title><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script></head><body>");
+        
+        echo("<H1><A href='cellphotoview.php'>View Cell Photos</a></h1>");
 
-<!--NOTE: /CELLPHOTO, /CELLPHOTO/CELLPHOTO, AND /CELLPHOTO/CELLPHOTO/THUMBS NEED TO BE WRITEABLE-->
-<H1><A href="cellphotoview.php">View Cell Photos</a></h1>
-<?php
-	
 	echo('CELLPHOTO<BR />');
         
         //get a cell photo mail object
-	$myCellPhotoMail = new CellPhotoMail(null,null,null,null,null);
+	$myCellPhotoMail = new CellPhotoMail(null,null,null);
         
         //check for and publish photos
         $myCellPhotoMail->ProcessPhotos();
         
-        
-	
-        
-?>
-</body>
-</html>
+        //html footer
+        echo("</body></html>");
+    }
