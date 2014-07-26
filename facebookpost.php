70556360c46a6676fb75f607676a13fc4ef66b9a
jimarasim <jaemzware@hotmail.com>
2014-07-26 12:43:01 -0700

    got timer apperatus working

--- facebookpost.php
+++ facebookpost.php
@@ -6,6 +6,11 @@
 </head>
 <body>
 <script>
+    
+    var intervalFunction; //set at runtime, so can be cleared
+    var timeout = 10000;
+    
+    
     window.fbAsyncInit = function() {
         FB.init({
           appId      : '709181395771740',
@@ -28,6 +33,10 @@
      fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
     
+    /**
+     * This method makes sure a user is authenticated, then does something if they are
+     * @returns {undefined}
+     */
     function checkAuthenticationStatus()
     {
 //        $('#analogplaylist').empty();
@@ -42,13 +51,18 @@
           var uid = response.authResponse.userID;
           var accessToken = response.authResponse.accessToken;
 
-//          $('#fbstatuslist').empty();
+          //print info of user authenticated
           $('#fbstatuslist').append("<li>UID:"+uid+"</li>");
           $('#fbstatuslist').append("<li>TOKEN:"+accessToken+"</li>");
           
           FB.api('/me', {fields: 'last_name,first_name'}, function(response) {
-                $('#fbstatuslist').append("<li>"+response.first_name+" "+response.last_name+"</li>");
-                postStatusToFacebookOmlb('sk8creteordie http://seattlerules.com/cellphoto/cellphoto/JAEMZBOT201404191310031.jpg');
+                var timestamp = new Date();
+                $('#fbstatuslist').append("<li>"+response.first_name+" "+response.last_name+" "+timestamp+"</li>");
+                
+                //kick off the interval
+                intervalFunction = setInterval(function(){intervalThread()},timeout);
+                                    
+
           });
 
         } else if (response.status === 'not_authorized') {
@@ -65,6 +79,11 @@
        });
     }
     
+    /**
+     * This method will prompt the user to log into facebook, and get the 
+     * required permissions from them
+     * @param {type} statusMessage
+     * @returns {undefined}     */
     function loginToFacebook()
     {
         FB.login(function(response) {
@@ -74,24 +93,31 @@
                 checkAuthenticationStatus();
               });
             } else {
-//                $('#fbstatuslist').empty();
                 $('#fbstatuslist').append("<li>USER CANCELLED LOGIN OR DID NOT AUTHORIZE</li>");
             }
           }, {scope: 'publish_actions, user_groups'});
     }
     
+    /**
+     * This method will post a status on the users behalf, with a timestamp
+     * @param {type} statusMessage
+     * @returns {undefined}     */
     function postStatusToFacebook(statusMessage){
         var timestamp = new Date();
         statusMessage += '('+timestamp+')';
         FB.api('/me/feed', 'post', { message: statusMessage }, function(response) {
           if (!response || response.error) {
-            $('#fbstatuslist').append('<li>Post Error occured:'+response.error.message+'</li>');
+            $('#fbstatuslist').append('<li>postStatusToFacebook Error occured:'+response.error.message+' '+timestamp+'</li>');
+            stopInterval();
           } else {
-            $('#fbstatuslist').append('<li>Post ID: ' + response.id+'</li>');
+            $('#fbstatuslist').append('<li>postStatusToFacebook ID: ' + response.id + ' '+timestamp+'</li>');
           }
         });
     }
     
+    /** This method will post a status on the users behalf to the ombl group
+     * @param {type} statusMessage
+     * @returns {undefined}     */
     function postStatusToFacebookOmlb(statusMessage){
         //omlb group id: 191968037495092
         //get this by:
@@ -102,13 +128,39 @@
         statusMessage += '('+timestamp+')';
         FB.api('/191968037495092/feed', 'post', { message: statusMessage }, function(response) {
           if (!response || response.error) {
-            $('#fbstatuslist').append('<li>Post Error occured:'+response.error.message+'</li>');
+            $('#fbstatuslist').append('<li>postStatusToFacebookOmlb Error occured:'+response.error.message+' '+timestamp+'</li>');
+            stopInterval();
           } else {
-            $('#fbstatuslist').append('<li>Post ID: ' + response.id+'</li>');
+            $('#fbstatuslist').append('<li>postStatusToFacebookOmlb ID: ' + response.id + ' '+timestamp+'</li>');
           }
         });
     }
     
+    
+
+    /**
+     * Thread that gets executed at every timeout, once the facebook user has been authenticated
+     * @returns {undefined}     */
+    function intervalThread()
+    {
+        //postStatusToFacebookOmlb('sk8creteordie http://seattlerules.com/cellphoto/cellphoto/JAEMZBOT201404191310031.jpg');
+        var timestamp = new Date();
+        $('#fbstatuslist').append("<li>timer interval"+timestamp+"<input type='button' onclick='stopInterval()' value='Stop timer'  /></li>");
+        $.get( "cellphoto.php?jsonImageLinks", function( data ) {
+            $('#fbstatuslist').append('<li>Cellphotos:'+data+'</li>');
+          });
+        
+    }
+    
+    /**
+     * This function will kill the timer if any error occurs
+     * @returns {undefined}     */
+    function stopInterval()
+    {
+        var timestamp = new Date();
+        $('#fbstatuslist').append('<li>Stopping timer. Refresh page to re-authenticate '+timestamp+'</li>');
+        window.clearTimeout(intervalFunction);
+    }
 </script>
 <ul id="fbstatuslist">
 </ul>
